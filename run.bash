518146d1601c9e34693890f8ed6e5534d28053b7
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-11-15 01:55:40 +0900

    update run.bash

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 THIS_DIR="$PWD"
 
-docker run --name 73b2-kitchen-edgetpu \
+docker run --name 73b2-kitchen-edgetpu --gpus all \
 --rm -it --privileged -p 6006:6006 \
 --mount type=bind,src=${THIS_DIR}/learn,dst=/tensorflow/models/research/73b2_kitchen_learn \
 --mount type=bind,src=${THIS_DIR}/scripts,dst=/tensorflow/models/research/73b2_kitchen_scripts \
