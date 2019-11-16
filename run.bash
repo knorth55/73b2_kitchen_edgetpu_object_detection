cc6296d093d35b97c50cbec46e5f65509b3054ac
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-11-17 04:25:14 +0900

    update run.sh

--- run.bash
+++ run.bash
@@ -1,7 +1,8 @@
 THIS_DIR="$PWD"
 
-docker run --name 73b2-kitchen-edgetpu --gpus all \
---rm -it --privileged -p 6006:6006 \
---mount type=bind,src=${THIS_DIR}/learn,dst=/tensorflow/models/research/73b2_kitchen_learn \
---mount type=bind,src=${THIS_DIR}/scripts,dst=/tensorflow/models/research/73b2_kitchen_scripts \
-73b2-kitchen-object-detection
+docker run --name 73b2-kitchen-edgetpu \
+    --gpus all \
+    --rm -it --privileged -p 6006:6006 \
+    --mount type=bind,src=${THIS_DIR}/learn,dst=/tensorflow/models/research/73b2_kitchen_learn \
+    --mount type=bind,src=${THIS_DIR}/scripts,dst=/tensorflow/models/research/73b2_kitchen_scripts \
+    73b2-kitchen-object-detection
